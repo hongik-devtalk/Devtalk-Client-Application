232d9cc335eb67692e5df596905999873d56f737
YuminPark <p353700@gmail.com>
2025-09-22 02:14:20 +0900

    #8 [ADD] dev 로고 추가

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,5 +1,32 @@
-import React from 'react';
+import link from '../../assets/icons/link.svg';
+import mail from '../../assets/icons/mail.svg';
+import messagecircle from '../../assets/icons/messagecircle.svg';
 
-export const Footer = () => {
-  return <div>Footer</div>;
+const Footer = () => {
+  return (
+    <footer className="flex justify-between items-start w-[37.5rem] h-[12.2rem] bg-grey-800">
+      <div className="justify-between w-[32.7rem] h-[6.2rem]">
+        {/* 왼쪽 정보 영역 */}
+        <div className="flex flex-col gap-4">
+          <div className="flex items-center gap-[0.8rem]">
+            <img src={link} alt="link" className="w-[1.8rem] h-[1.8rem]" />
+            <span className="font-caption-medium text-grey-200">개인정보 처리 방침 링크</span>
+          </div>
+          <div className="flex items-center gap-[0.8rem]">
+            <img src={mail} alt="mail" className="w-[1.8rem] h-[1.8rem]" />
+            <span className="font-caption-medium text-grey-200">devtalk11@gmail.com</span>
+          </div>
+          <div className="flex items-center gap-[0.8rem]">
+            <img src={messagecircle} alt="messagecircle" className="w-[1.8rem] h-[1.8rem]" />
+            <span className="font-caption-medium text-grey-200">문의하기</span>
+          </div>
+        </div>
+
+        {/* 오른쪽 로고 */}
+        <div></div>
+      </div>
+    </footer>
+  );
 };
+
+export default Footer;
