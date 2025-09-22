c7e58e679c08c6ddcdd4f891210c9c8c95a4f270
김준희 <shadd3338@gmail.com>
2025-09-22 10:20:45 +0900

    #7 [FEAT] admin 페이지 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import SeminarHome from './pages/seminar/SeminarHome';
 import SeminarDetail from './pages/seminar/SeminarDetail';
+import Admin from './pages/admin/Admin';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/seminar" element={<SeminarHome />} />
         <Route path="/seminar/:id" element={<SeminarDetail />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/admin/Admin.tsx
@@ -0,0 +1,9 @@
+function Admin() {
+  return (
+    <div>
+      <h1>admin 페이지</h1>
+    </div>
+  );
+}
+
+export default Admin;
