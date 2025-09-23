484a258c63cda4087c8a4eb1ba2fce74f5897478
chaeyoon <codbs0722@gmail.com>
2025-09-23 14:04:57 +0900

    [UI]: 세션 텍스트 UI 수정

--- src/components/LectureCard/LectureCardSession.tsx
+++ src/components/LectureCard/LectureCardSession.tsx
@@ -17,21 +17,23 @@ export const LectureCardSession = () => {
 
       {/* 세션 내용 */}
       <div className="body-2-medium text-grey-200 text-left">
-        <p className="text-gradient">ChatGPT 3년차,</p>
         <p>
-          LLM은 더욱 어려운 문제를 해결하고 실제 작업을 수행하는 수준으로 발전했습니다.
-          <br />
+          <span className="text-gradient">ChatGPT 3년차,</span> LLM은 더욱 어려운 문제를 해결하고
+          실제 작업을 수행하는 수준으로 발전했습니다.
         </p>
         <br />
         <p>
           코딩을 비롯한 다양한 분야에서 인간의 능력을 넘어서고, 연구 보고서도 쓰고, 혼자 티켓 예약도
-          할 수 있다는데요.
+          할 수 있다는데요. <br />
+          <span className="text-gradient">LLM은 어쩌다 이렇게 똑똑해졌을까요?</span>
         </p>
-        <p className="text-gradient">LLM은 어쩌다 이렇게 똑똑해졌을까요?</p>
+
         <br />
-        <p>LLM의 놀라운 능력의 비밀,</p>
-        <p className="text-gradient">추론(Reasoning)과 에이전트(Agent)</p>
-        <p>라는 핵심 키워드를 쉽고 명확하게 알아봅시다!</p>
+        <p>
+          LLM의 놀라운 능력의 비밀,{' '}
+          <span className="text-gradient">추론(Reasoning)과 에이전트(Agent)</span>라는 핵심 키워드를
+          쉽고 명확하게 알아봅시다!
+        </p>
       </div>
     </div>
   );
