2a2a93b2133a36ae68498db6477998b85eccb7b0
YuminPark <p353700@gmail.com>
2025-09-23 12:26:32 +0900

    #11 [FEAT] Header 컴포넌트 구현

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,5 +1,15 @@
+import hamburger from '../../assets/icons/components/Header/hamburger.svg';
+import devlogo from '../../assets/logos/devlogo.svg';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="flex justify-center items-center w-full h-[56px] bg-black">
+      <div className="flex items-center justify-between w-[335px] h-[34px]">
+        <img src={devlogo} alt="devlogo" className="w-[77px] h-[32px]" />
+        <img src={hamburger} alt="hamburger" className="w-[24px] h-[24px]" />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
