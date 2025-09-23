049f82d373842cdf848859991c82d655b7753a00
chaeyoon <codbs0722@gmail.com>
2025-09-24 00:45:32 +0900

    #22 [UI] 신청하기 버튼 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import './App.css';
+import { Button } from './components/Button/Button';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="flex flex-col gap-8 pt-10">
+        <Button variant="home" />
+        <Button variant="default" />
+        <Button variant="sub" />
+        <Button variant="disabled" />
+      </div>
+    </>
+  );
 }
 
 export default App;
