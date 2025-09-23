d54891cc9043aadd17ca40118518ed70b4a23d55
chaeyoon <codbs0722@gmail.com>
2025-09-24 01:36:40 +0900

    #22 [CHORE] App.tsx, index.css 원상 복구

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
 import './App.css';
-import { Button } from './components/Button/Button';
-import { ButtonExSeminar } from './components/Button/ButtonExSeminar';
-import { ChipSeminar } from './components/Chip/ChipSeminar';
-import { ChipTimePlace } from './components/Chip/ChipTimePlace';
 
 function App() {
-  return (
-    <>
-      <div className="flex flex-col gap-8 pt-10">
-        <Button variant="home" />
-        <Button variant="default" />
-        <Button variant="sub" />
-        <Button variant="disabled" />
-        <ButtonExSeminar />
-        <ChipSeminar />
-        <ChipTimePlace />
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
