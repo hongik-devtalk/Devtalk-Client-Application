e8102f3e299b4e0197dd15e35e606bbefd0b4c02
Junseok Ahn <rrvvy9595@gmail.com>
2025-09-23 18:47:30 +0900

    #12 [CHORE] App.tsx 원상 복구

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import AdminLayout from './layouts/admin/AdminLayout';
-import PromoImage from './pages/admin/PromoImage';
+
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route path="/admin/home/promo" element={<PromoImage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <></>;
 }
 
 export default App;
