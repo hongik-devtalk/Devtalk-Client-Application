25ecc54ccdf5cf68526ae23fe7114a283e2480c5
Junseok Ahn <rrvvy9595@gmail.com>
2025-09-23 13:39:21 +0900

    #12 [FEAT] Admin의 레이아웃 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import './App.css';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AdminLayout from './layouts/admin/AdminLayout';
+import PromoImage from './pages/admin/PromoImage';
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="/admin/home/promo" element={<PromoImage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/admin/PromoImage.tsx
@@ -0,0 +1,5 @@
+const PromoImage = () => {
+  return <div>홍보 사진 관리</div>;
+};
+
+export default PromoImage;
