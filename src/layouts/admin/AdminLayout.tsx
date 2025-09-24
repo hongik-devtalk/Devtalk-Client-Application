e540dc648274c5290ebebaeabf6bc427b8a8ac7c
Junseok Ahn <rrvvy9595@gmail.com>
2025-09-24 14:25:41 +0900

    #23 [ADD] 레이아웃에 scrollbar-hide 추가

--- src/layouts/admin/AdminLayout.tsx
+++ src/layouts/admin/AdminLayout.tsx
@@ -6,7 +6,7 @@ const AdminLayout: React.FC = () => {
   return (
     <div className="flex h-screen bg-background">
       <Sidebar />
-      <main className="flex-1 overflow-y-auto p-8">
+      <main className="flex-1 overflow-y-auto p-8 scrollbar-hide">
         <Outlet />
       </main>
     </div>
