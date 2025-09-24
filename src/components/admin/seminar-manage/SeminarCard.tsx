b88b84b9ba8e0096ec9e7d253b6441b7ef3a298c
Junseok Ahn <rrvvy9595@gmail.com>
2025-09-24 14:41:40 +0900

    #23 [FEAT] 세미나 카드 조회 구현

---
+++ src/components/admin/seminar-manage/SeminarCard.tsx
@@ -0,0 +1,49 @@
+import { Link } from 'react-router-dom';
+import { formatDate } from '../../../utils/formatDate';
+
+interface Seminar {
+  id: number;
+  title: string;
+  date: string;
+  location: string;
+  topic: string;
+}
+
+interface SeminarCardProps {
+  seminar: Seminar;
+}
+
+const SeminarCard: React.FC<SeminarCardProps> = ({ seminar }) => {
+  return (
+    <Link to={`/admin/seminars/${seminar.id}`}>
+      <div className="bg-grey-700 rounded-lg p-5 cursor-pointer transition-shadow hover:shadow-2xl shadow-grey-500">
+        {/* 세미나 정보 */}
+        <h2 className="heading-2-bold mb-3 text-center">{seminar.title}</h2>
+
+        <div className="flex space-x-5">
+          {/* 이미지 */}
+          <div className="w-[250px] h-[200px] bg-grey-100 rounded-md"></div>
+
+          <div className="flex flex-col mt-[16px] flex-1 min-w-0">
+            <div className="mb-[16px]">
+              <p className="body-2-semibold mb-[8px] text-grey-300">일정</p>
+              <div className="caption-semibold">{formatDate(seminar.date)}</div>
+            </div>
+            <div className="mb-[16px]">
+              <p className="body-2-semibold mb-[8px] text-grey-300">장소</p>
+              <div className="caption-semibold ">{seminar.location}</div>
+            </div>
+            <div className="mb-[16px]">
+              <p className="body-2-semibold mb-[8px] text-grey-300">주제</p>
+              <div className="caption-semibold w-full overflow-hidden whitespace-nowrap text-ellipsis">
+                {seminar.topic}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default SeminarCard;
