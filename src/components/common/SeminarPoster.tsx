dfb07260e91badfcdc14bd066925f05ad0d0db91
chaeyoon <codbs0722@gmail.com>
2025-09-25 02:06:05 +0900

    #42 [UI] 홈 UI 구현

--- src/components/common/SeminarPoster.tsx
+++ src/components/common/SeminarPoster.tsx
@@ -2,12 +2,12 @@ import seminarPoster from '../../assets/seminarPoster.svg';
 
 const SeminarPoster = () => {
   return (
-    <div className="relative w-[376px] h-[585px]">
+    <div className="relative w-[376px] h-[480px]">
       {/* 배경 이미지 */}
       <img src={seminarPoster} alt="세미나 포스터" className="w-full h-full object-cover" />
 
       {/* 텍스트 */}
-      <div className="absolute top-0 left-[20px] flex flex-col w-[335px] h-[196px] gap-[36px] p-4">
+      <div className="absolute top-0 pt-[32px] pl-[20px] flex flex-col w-[335px] h-[196px] gap-[36px] p-4">
         <div className="flex flex-col gap-[4px]">
           <p className="text-grey-50 subhead-2-medium">10회차</p>
           <p className="heading-1-bold text-gradient">LLM을 파헤치다</p>
