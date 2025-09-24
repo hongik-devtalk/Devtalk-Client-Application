46567916146ea2ba597096fc6b601d94bf66e737
YuminPark <p353700@gmail.com>
2025-09-24 23:36:13 +0900

    #35 [FEAT] 신청하기 헤더 구현

--- src/components/SeminarApply/ApplyHeader.tsx
+++ src/components/SeminarApply/ApplyHeader.tsx
@@ -1,6 +1,13 @@
+import arrow from "../../assets/icons/components/SeminarApply/arrow.svg"
+
 const ApplyHeader = () => {
   return (
-    <div>Header</div>
+    <header className="flex items-center w-full h-[56px] bg-black px-20">
+      <div className="flex items-center justify-between w-50 h-7">
+        <img src={arrow} alt="arrow" className="w-7 h-7" />
+        <div className="subhead-2-medium text-white">신청하기</div>
+      </div>
+    </header>
   )
 }
 
