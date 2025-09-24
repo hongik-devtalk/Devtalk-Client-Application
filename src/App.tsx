2a2da7e1843d696a85fe6042531e089836f57d7b
조수진 <lavesujin@gmail.com>
2025-09-24 15:22:03 +0900

    #24 [UI] CTA 상단 멘트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Cta from './components/common/Cta';
 
 function App() {
-  return <></>;
+  return (
+    <div className="bg-#343434">
+      <Cta />
+    </div>
+  );
 }
 
 export default App;
