1776931177cedfb18babba4bd961416af17c1ab0
wlsldm <lavesujin@gmail.com>
2025-09-25 17:37:14 +0900

    #47 [MOVE] home/Home.tsx  -> seminar/Home.tsx로 코드 이동

--- src/pages/user/seminar/Home.tsx
+++ src/pages/user/seminar/Home.tsx
@@ -1,9 +1,24 @@
+import Header from '../../../components/common/Header';
 import SeminaListCard from '../../../components/Semina/SeminaListCard';
 
 function SeminarHome() {
   return (
     <div>
-      <SeminaListCard />
+      <div className="flex flex-col justify-center gap-16 px-20">
+        <Header />
+        <div className="heading-1-bold text-white">세미나</div>
+        <div className="flex flex-col gap-28 items-center ">
+          <div className="border-t border-grey-700">
+            <SeminaListCard />
+          </div>
+          <div className="border-t border-grey-700">
+            <SeminaListCard />
+          </div>
+          <div className="border-t border-grey-700">
+            <SeminaListCard />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
