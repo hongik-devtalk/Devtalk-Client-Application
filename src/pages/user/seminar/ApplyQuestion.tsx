61a524783d324df7d7fed501e789d3d2618f5c5a
YuminPark <p353700@gmail.com>
2025-09-26 02:47:05 +0900

    #48 [CHORE] 신청하기 페이지 간격 조정 및 연결

--- src/pages/user/seminar/ApplyQuestion.tsx
+++ src/pages/user/seminar/ApplyQuestion.tsx
@@ -3,10 +3,21 @@ import SpeakerCard from '../../../components/SeminarApply/SpeakerCard';
 
 const ApplyQuestion = () => {
   return (
-    <>
+    <div className="flex flex-col">
       <ApplyHeader backTo="/seminar/apply-info" />
-      <SpeakerCard />
-    </>
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-1 px-5">
+          <div className="flex flex-row gap-1">
+            <div className="heading-2-bold text-gradient">(선택)</div>
+            <div className="heading-2-bold text-white">사전 질문란</div>
+          </div>
+          <div className="body-2-medium text-grey-300">
+            연사님께서 질문 선별 후, Q&A 시간에 답변 드립니다.
+          </div>
+        </div>
+        <SpeakerCard />
+      </div>
+    </div>
   );
 };
 
