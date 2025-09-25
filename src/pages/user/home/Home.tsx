449527393879855a47e457402e716cf845959cc7
wlsldm <lavesujin@gmail.com>
2025-09-25 17:14:39 +0900

    [UI] 세미나 홈 ui 구현

--- src/pages/user/home/Home.tsx
+++ src/pages/user/home/Home.tsx
@@ -1,7 +1,22 @@
+import Header from '../../../components/common/Header';
+import SeminaListCard from '../../../components/Semina/SeminaListCard';
+
 function Home() {
   return (
-    <div>
-      <h1>홈 페이지</h1>
+    <div className="flex flex-col justify-center gap-16 px-20">
+      <Header />
+      <div className="heading-1-bold text-white">세미나</div>
+      <div className="flex flex-col gap-28 items-center ">
+        <div className="border-t border-grey-700">
+          <SeminaListCard />
+        </div>
+        <div className="border-t border-grey-700">
+          <SeminaListCard />
+        </div>
+        <div className="border-t border-grey-700">
+          <SeminaListCard />
+        </div>
+      </div>
     </div>
   );
 }
