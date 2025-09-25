06df0f7abf6ae8522d035de6e58e5ce3fda30e28
wlsldm <lavesujin@gmail.com>
2025-09-25 17:26:11 +0900

    #47 [MOD] 세미나 리스트 카드 props 받는 형태로 변경

--- src/components/Semina/SeminaListCard.tsx
+++ src/components/Semina/SeminaListCard.tsx
@@ -1,26 +1,33 @@
 import { ChipSeminar } from '../Chip/ChipSeminar';
 
-const SeminaListCard = () => {
+interface SeminarItem {
+  title: string;
+  date: string;
+  time: string;
+  location: string;
+  image: string;
+}
+// 임의로 작성 -> 추후 API 연동 시 변경 필요
+
+const SeminaListCard = ({ title, date, time, location, image }: SeminarItem) => {
   return (
     <div className="w-[335px] h-[263px] pt-20 pb-24 gap-20 flex flex-col">
       <div className="h-[93px] flex flex-col gap-12">
         <ChipSeminar />
-        <div className="heading-3-semibold text-white">
-          세미나 제목 영역입니다 <br /> 세미나 제목 영역입니다아아
-        </div>
+        <div className="heading-3-semibold text-white">{title}</div>
       </div>
       <div className="h-[106px] flex flex-row gap-16">
-        <img src="/" alt="seminar Img" className="w-[150px] h-[106px] rounded-8" />
+        <img src={image} alt="seminar Img" className="w-[150px] h-[106px] rounded-8" />
         <div className="h-[68px] flex flex-col gap-8 justify-between body-2-medium">
           <div className="flex flex-row gap-20 ailgn-start">
             <div className="text-grey-300 whitespace-nowrap">일정</div>
             <div className="text-grey-400">
-              2025. 10. 04.(토) <br /> 오후 7:00
+              {date} <br /> {time}
             </div>
           </div>
           <div className="flex flex-row gap-20 ailgn-start">
             <div className="text-grey-300">장소</div>
-            <div className="text-grey-400">가나다라마바사</div>
+            <div className="text-grey-400">{location}</div>
           </div>
         </div>
       </div>
