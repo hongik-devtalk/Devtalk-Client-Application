fa61823350532a5f8c1e507c95b8d04d862cf83f
김준희 <shadd3338@gmail.com>
2025-09-26 03:03:15 +0900

    #31 [UI] 어드민 세미나 신청자 관리 페이지 구현

--- src/pages/admin/seminar-manage/applicants/List.tsx
+++ src/pages/admin/seminar-manage/applicants/List.tsx
@@ -1,7 +1,10 @@
+import ApplicantsList from '../../../../components/admin/applicants/ApplicantsList';
+
 const List = () => {
   return (
-    <div>
-      <h1>세미나 신청자 관리 메인페이지</h1>
+    <div className="py-11">
+      <h1 className="ml-[39px] text-white font-['Inter'] text-[32px] font-semibold leading-normal mb-12">세미나 신청자 관리</h1>
+      <ApplicantsList />
     </div>
   );
 };
