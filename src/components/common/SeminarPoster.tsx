3746e16ea2d678ba5bb05264d96621ce44bf6470
chaeyoon <codbs0722@gmail.com>
2025-09-25 23:06:43 +0900

    #42 [UI] 포스터 간격 수정

--- src/components/common/SeminarPoster.tsx
+++ src/components/common/SeminarPoster.tsx
@@ -2,27 +2,29 @@ import seminarPoster from '../../assets/seminarPoster.svg';
 
 const SeminarPoster = () => {
   return (
-    <div className="relative w-[376px] h-[480px]">
+    <div className="relative w-[376px] h-[585px]">
       {/* 배경 이미지 */}
       <img src={seminarPoster} alt="세미나 포스터" className="w-full h-full object-cover" />
 
       {/* 텍스트 */}
-      <div className="absolute top-0 pt-32 pl-20 flex flex-col w-[335px] h-[196px] gap-36 p-4">
+      <div className="absolute top-0 pt-32 pl-20 flex flex-col justify-center w-[335px] h-[196px]">
         <div className="flex flex-col gap-4">
           <p className="text-grey-50 subhead-2-medium">10회차</p>
           <p className="heading-1-bold text-gradient">LLM을 파헤치다</p>
         </div>
 
-        <div className="flex gap-28">
-          <div className="flex flex-col text-grey-300 body-1-medium">
-            <p>대상</p>
-            <p>일시</p>
-            <p>장소</p>
+        <div className="flex flex-col gap-8 body-1-medium pt-36">
+          <div className="flex gap-28">
+            <p className="text-grey-300">대상</p>
+            <p className="text-grey-200">홍익대학교 재학생 (학과/학년 무관)</p>
           </div>
-          <div className="flex flex-col text-grey-200 body-1-medium">
-            <p>홍익대학교 재학생 (학과/학년 무관)</p>
-            <p>2025. 10. 4(토) 오후 6:30~8:30</p>
-            <p>홍익대학교 L0201</p>
+          <div className="flex gap-28">
+            <p className="text-grey-300">일시</p>
+            <p className="text-grey-200">2025. 10. 4.(토) 오후 6:30~8:30</p>
+          </div>
+          <div className="flex gap-28">
+            <p className="text-grey-300">장소</p>
+            <p className="text-grey-200">홍익대학교 L0201</p>
           </div>
         </div>
       </div>
