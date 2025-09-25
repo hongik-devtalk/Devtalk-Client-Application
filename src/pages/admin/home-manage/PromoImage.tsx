1b1357f59456040c3649cd8aee70507c7fef5f40
jungyungee <jungyungee0610@gmail.com>
2025-09-25 20:39:54 +0900

    #44 [CHORE] 파일 원상복구 수정

--- src/pages/admin/home-manage/PromoImage.tsx
+++ src/pages/admin/home-manage/PromoImage.tsx
@@ -4,6 +4,15 @@ const PromoImage = () => {
   return (
     <div>
       <h1>홍보사진관리</h1>
+      <AdminImageUpload
+        title="Devtalk 소개 사진"
+        onUpload={(files) => console.log('이미지 업로드:', files)}
+        onRemove={(index) => console.log('이미지 삭제:', index)}
+      />
+      <AdminPresentationUpload
+        onUpload={(files) => console.log('발표자료 업로드:', files)}
+        onRemove={(index) => console.log('발표자료 삭제:', index)}
+      />
     </div>
   );
 };
