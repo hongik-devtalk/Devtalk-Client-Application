ea3e4d02ff248a12f0524ac5b08169b641ec8601
jungyungee <jungyungee0610@gmail.com>
2025-09-25 20:43:19 +0900

    #44 [CHORE] 수정 사항 반영

--- src/pages/admin/home-manage/PromoImage.tsx
+++ src/pages/admin/home-manage/PromoImage.tsx
@@ -1,18 +1,7 @@
-import AdminImageUpload from '../../../components/admin/upload/AdminImageUpload';
-import AdminPresentationUpload from '../../../components/admin/upload/AdminPresentationUpload';
 const PromoImage = () => {
   return (
     <div>
       <h1>홍보사진관리</h1>
-      <AdminImageUpload
-        title="Devtalk 소개 사진"
-        onUpload={(files) => console.log('이미지 업로드:', files)}
-        onRemove={(index) => console.log('이미지 삭제:', index)}
-      />
-      <AdminPresentationUpload
-        onUpload={(files) => console.log('발표자료 업로드:', files)}
-        onRemove={(index) => console.log('발표자료 삭제:', index)}
-      />
     </div>
   );
 };
