a0a44b415a0a6296d88df7ec8fc5f75a1760385f
Junseok Ahn <rrvvy9595@gmail.com>
2025-09-27 03:08:22 +0900

    #61 [UI] 세미나 라이브 링크 구현

---
+++ src/components/admin/SeminarManage/LiveLinkInput.tsx
@@ -0,0 +1,27 @@
+interface LiveLinkInputProps {
+  link: string;
+  onLinkChange: (newLink: string) => void;
+}
+
+const LiveLinkInput: React.FC<LiveLinkInputProps> = ({ link, onLinkChange }) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onLinkChange(e.target.value);
+    console.log(e.target.value);
+  };
+
+  return (
+    <div>
+      <h2 className="heading-2-bold text-white mb-6">세미나 Live 링크</h2>
+      <input
+        type="url"
+        className="w-full h-[66px] px-6 py-5 rounded-[var(--radius-8)] bg-grey-700 text-grey-300  
+                   focus:outline-none focus:ring-2 focus:ring-green-300 border-transparent"
+        placeholder="링크 URL을 입력해주세요."
+        value={link}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+export default LiveLinkInput;
