cce0cebcd77a2e59a826ebc8ff485667d5cc540e
YuminPark <p353700@gmail.com>
2025-09-27 21:52:59 +0900

    #48 [CHORE] textarea 입력창 연결

--- src/pages/user/seminar/ApplyQuestion.tsx
+++ src/pages/user/seminar/ApplyQuestion.tsx
@@ -5,17 +5,20 @@ const ApplyQuestion = () => {
   return (
     <div className="flex flex-col">
       <ApplyHeader backTo="/seminar/apply-info" />
-      <div className="flex flex-col gap-8">
-        <div className="flex flex-col gap-1 px-5">
-          <div className="flex flex-row gap-1">
-            <div className="heading-2-bold text-gradient">(선택)</div>
-            <div className="heading-2-bold text-white">사전 질문란</div>
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-8">
+          <div className="flex flex-col gap-1 px-5">
+            <div className="flex flex-row gap-1">
+              <div className="heading-2-bold text-gradient">(선택)</div>
+              <div className="heading-2-bold text-white">사전 질문란</div>
+            </div>
+            <div className="body-2-medium text-grey-300">
+              연사님께서 질문 선별 후, Q&A 시간에 답변 드립니다.
+            </div>
           </div>
-          <div className="body-2-medium text-grey-300">
-            연사님께서 질문 선별 후, Q&A 시간에 답변 드립니다.
-          </div>
+          <SpeakerCard />
         </div>
-        <SpeakerCard />
+        <textarea className="mx-5 rounded-8 bg-grey-800" />
       </div>
     </div>
   );
