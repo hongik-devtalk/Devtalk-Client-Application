ab9275ab364116462a0561c0921e1609bf08d579
Junseok Ahn <rrvvy9595@gmail.com>
2025-10-07 03:05:30 +0900

    #138 [FEAT] SeminarDetailData를 SeminarDetailState로 변환하는 매핑 함수 구현

---
+++ src/utils/seminarMapper.ts
@@ -0,0 +1,32 @@
+import type { SeminarDetailData, SpeakerData } from '../types/SeminarManage/seminarDetail.api';
+import type { SeminarDetailState, SpeakerState } from '../types/SeminarManage/seminar.state';
+
+export const mapApiDataToState = (apiData: SeminarDetailData): SeminarDetailState => {
+  return {
+    seminarId: apiData.seminarId,
+    seminarNum: apiData.seminarNum,
+    topic: apiData.topic,
+    seminarDate: apiData.seminarDate,
+    place: apiData.place,
+    liveLink: apiData.liveLink,
+    thumbnailUrl: apiData.thumbnail?.fileUrl || null,
+    thumbnailFileName: apiData.thumbnail?.fileName || null,
+    materials: apiData.materials || [],
+    speakers: apiData.speakers.map(
+      (apiSpeaker: SpeakerData): SpeakerState => ({
+        speakerId: apiSpeaker.speakerId,
+        name: apiSpeaker.name,
+        organization: apiSpeaker.organization,
+        history: apiSpeaker.history,
+        sessionTitle: apiSpeaker.sessionTitle,
+        sessionContent: apiSpeaker.sessionContent,
+        profileUrl: apiSpeaker.profile?.fileUrl || null,
+        profileFileName: apiSpeaker.profile?.fileName || null,
+      })
+    ),
+    seminarStartDate: new Date(apiData.activeStartDate),
+    seminarEndDate: new Date(apiData.activeEndDate),
+    applicationStartDate: new Date(apiData.applyStartDate),
+    applicationEndDate: new Date(apiData.applyEndDate),
+  };
+};
