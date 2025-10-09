ade178254af701678ce320ba8ec6a65389edc7a3
YuminPark <p353700@gmail.com>
2025-10-09 16:52:07 +0900

    #176 [DEL] X 아이콘 제거

--- src/components/common/HamburgerBar.tsx
+++ src/components/common/HamburgerBar.tsx
@@ -1,12 +1,11 @@
 import { useNavigate } from 'react-router-dom';
-import deleteIcon from '../../assets/icons/common/delete.svg';
 
 type HamburgerBarProps = {
   isOpen: boolean;
   onClose: () => void;
 };
 
-const HamburgerBar = ({ isOpen, onClose }: HamburgerBarProps) => {
+const HamburgerBar = ({ isOpen, onClose: _onClose }: HamburgerBarProps) => {
   const navigate = useNavigate();
 
   const handleIntroduceClick = () => {
@@ -34,12 +33,6 @@ const HamburgerBar = ({ isOpen, onClose }: HamburgerBarProps) => {
         className={`absolute w-[375px] h-screen bg-black transform transition-transform duration-400 z-40 ${isOpen ? 'translate-y-0' : '-translate-y-full'}`}
       >
         <div className="flex flex-col gap-16 pt-60 px-20">
-          <div className="flex items-center justify-end">
-            <button onClick={onClose} className="">
-              <img src={deleteIcon} alt="닫기 버튼" className="w-[24px] h-[24px] cursor-pointer" />
-            </button>
-          </div>
-
           <nav className="flex flex-col text-grey-300 subhead-1-semibold">
             <button
               className="w-[335px] py-[12px] pl-[16px] pr-[8px] rounded-8 text-left hover:bg-grey-800 hover:text-white cursor-pointer transition-all duration-200"
