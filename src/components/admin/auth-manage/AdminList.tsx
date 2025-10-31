f58de272d081d8bd34918e5cffb54c780902d1fe
jungyungee <jungyungee0610@gmail.com>
2025-10-31 19:38:38 +0900

    #225 [UI] 모달 창 구현

--- src/components/admin/auth-manage/AdminList.tsx
+++ src/components/admin/auth-manage/AdminList.tsx
@@ -1,18 +1,32 @@
+import { useState } from 'react';
+import AuthDeleteModal from '../auth-manage/AuthDeleteModal';
+
 const AdminList = () => {
   const admins = [
-    { id: 1, name: '데브톡', username: 'devtalk1' },
-    { id: 2, name: '김데브', username: 'deeev2' },
-    { id: 3, name: '이데브', username: 'deeev3' },
-    { id: 4, name: '박데브', username: 'deeev4' },
-    { id: 5, name: '최데브', username: 'deeev5' },
-    { id: 6, name: '정데브', username: 'deeev6' },
-    { id: 7, name: '안데브', username: 'deeev7' },
-    { id: 8, name: '강데브', username: 'deeev8' },
-    { id: 9, name: '인데브', username: 'deeev9' },
-    { id: 10, name: '임데브', username: 'deeevvvvvv' },
-    { id: 11, name: '장데브', username: 'dee' },
-    { id: 12, name: '손데브', username: 'deeevasfd' },
+    { no: 1, name: '데브톡', id: 'devtalk1' },
+    { no: 2, name: '김데브', id: 'deeev2' },
+    { no: 3, name: '이데브', id: 'deeev3' },
+    { no: 4, name: '박데브', id: 'deeev4' },
+    { no: 5, name: '최데브', id: 'deeev5' },
+    { no: 6, name: '정데브', id: 'deeev6' },
+    { no: 7, name: '안데브', id: 'deeev7' },
+    { no: 8, name: '강데브', id: 'deeev8' },
+    { no: 9, name: '인데브', id: 'deeev9' },
+    { no: 10, name: '임데브', id: 'deeevvvvvv' },
+    { no: 11, name: '장데브', id: 'dee' },
+    { no: 12, name: '손데브', id: 'deeevasfd' },
   ];
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedAdmin, setSelectedAdmin] = useState<{
+    no: number;
+    name: string;
+    id: string;
+  } | null>(null);
+
+  const handleDelete = (no: number) => {
+    console.log(`관리자 ID ${no} 삭제`);
+  };
 
   return (
     <div className="w-full bg-grey-900 text-white rounded-10 overflow-hidden">
@@ -31,19 +45,38 @@ const AdminList = () => {
         <tbody>
           {admins.map((admin) => (
             <tr
-              key={admin.id}
+              key={admin.no}
               className="subhead-1-medium border-t border-grey-700 hover:bg-grey-800 transition-colors"
             >
-              <td className="py-20 px-20 text-center">{admin.id}</td>
+              <td className="py-20 px-20 text-center">{admin.no}</td>
               <td className="py-20 px-24">{admin.name}</td>
-              <td className="py-20 px-24">{admin.username}</td>
-              <td className="py-20 px-[44px] text-center text-status-error hover:text-shadow-status-error cursor-pointer">
+              <td className="py-20 px-24">{admin.id}</td>
+              <td
+                className="py-20 px-[44px] text-center text-status-error hover:text-shadow-status-error cursor-pointer"
+                onClick={() => {
+                  setSelectedAdmin(admin);
+                  setModalOpen(true);
+                }}
+              >
                 삭제
               </td>
             </tr>
           ))}
         </tbody>
       </table>
+
+      {selectedAdmin && (
+        <AuthDeleteModal
+          open={modalOpen}
+          adminId={selectedAdmin?.id ?? ''}
+          adminName={selectedAdmin?.name ?? ''}
+          onConfirm={() => {
+            handleDelete(selectedAdmin?.no ?? 0);
+            setModalOpen(false);
+          }}
+          onCancel={() => setModalOpen(false)}
+        />
+      )}
     </div>
   );
 };
