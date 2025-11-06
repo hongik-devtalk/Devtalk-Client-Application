31eddfdaf43d551017ae22331929de0fbd6e97d3
jungyungee <jungyungee0610@gmail.com>
2025-11-06 13:32:00 +0900

    #244 [CHORE] FAQ 타입 쓰도록 수정

--- src/hooks/HomeManage/useHomeFAQ.ts
+++ src/hooks/HomeManage/useHomeFAQ.ts
@@ -1,11 +1,11 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { getFAQLink, postFAQLink, deleteFAQLink } from '../../apis/HomeManage/homeFAQApi';
-import type { HomeLinkResponse, PostHomeLinkRequest } from '../../types/HomeManage/homeLink';
+import type { getFAQResponse, PostFAQRequest } from '../../types/HomeManage/homeFAQ';
 import { QUERY_KEYS } from '../../constants/queryKey';
 
 // 링크 조회
 export const useHomeFAQ = () => {
-  return useQuery<HomeLinkResponse>({
+  return useQuery<getFAQResponse>({
     queryKey: [QUERY_KEYS.ADMIN_HOME_FAQ],
     queryFn: getFAQLink,
   });
@@ -15,14 +15,14 @@ export const useHomeFAQ = () => {
 export const usePostHomeFAQ = () => {
   const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: (body: PostHomeLinkRequest) => postFAQLink(body),
+    mutationFn: (body: PostFAQRequest) => postFAQLink(body),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.ADMIN_HOME_FAQ] });
     },
   });
 };
 
-//링크 삭제
+// 링크 삭제
 export const useDeleteHomeFAQ = () => {
   const queryClient = useQueryClient();
   return useMutation({
