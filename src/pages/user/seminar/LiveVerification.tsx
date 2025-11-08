b18cafca6d4e43bc5d7bb4dd3df2dd70bf322c0a
chaeyoon <codbs0722@gmail.com>
2025-11-08 23:17:18 +0900

    #256 [UI] 라이브 인증 페이지 UI수정

--- src/pages/user/seminar/LiveVerification.tsx
+++ src/pages/user/seminar/LiveVerification.tsx
@@ -36,27 +36,29 @@ const LiveVerification = () => {
     <>
       <Header hamburgerOpen={hamburgerOpen} setHamburgerOpen={setHamburgerOpen} />
       {isPending && <LoadingSpinner />}
-      <div className="flex flex-col px-20 pt-28">
-        <p className="flex text-white heading-2-bold pb-32">세미나 신청자 인증</p>
-        <div className="flex flex-col gap-12 pb-[381px]">
-          <input
-            type="text"
-            placeholder="학번"
-            value={studentNum}
-            onChange={(e) => setStudentNum(e.target.value)}
-            className="w-[335px] h-[54px] px-16 py-8 text-grey-50 bg-grey-800 rounded-8 placeholder:text-grey-300 border border-transparent focus:border-grey-300 focus:outline-none"
-          />
-          <input
-            type="text"
-            placeholder="이름"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="w-[335px] h-[54px] px-16 py-8 text-grey-50 bg-grey-800 rounded-8 placeholder:text-grey-300 border border-transparent focus:border-grey-300 focus:outline-none"
-          />
+      <div className="pt-[56px]">
+        <div className="flex flex-col px-20 pt-28">
+          <p className="flex text-white heading-2-bold pb-32">세미나 신청자 인증</p>
+          <div className="flex flex-col gap-12 pb-[381px]">
+            <input
+              type="text"
+              placeholder="학번"
+              value={studentNum}
+              onChange={(e) => setStudentNum(e.target.value)}
+              className="w-[335px] h-[54px] px-16 py-8 text-grey-50 bg-grey-800 rounded-8 placeholder:text-grey-300 border border-transparent focus:border-grey-300 focus:outline-none"
+            />
+            <input
+              type="text"
+              placeholder="이름"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="w-[335px] h-[54px] px-16 py-8 text-grey-50 bg-grey-800 rounded-8 placeholder:text-grey-300 border border-transparent focus:border-grey-300 focus:outline-none"
+            />
+          </div>
         </div>
-      </div>
-      <div className="flex px-20 pt-[20px] pb-[89px]">
-        <Button text="인증하기" variant="default" onClick={handleSumbmit} />
+        <div className="flex px-20 pt-[20px] pb-[89px]">
+          <Button text="인증하기" variant="default" onClick={handleSumbmit} />
+        </div>
       </div>
     </>
   );
